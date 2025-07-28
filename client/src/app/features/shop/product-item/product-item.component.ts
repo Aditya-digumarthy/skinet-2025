b4741e4af5498531db89bf3502e549ced2446f06
Aditya-9936 <185909627+Aditya-digumarthy@users.noreply.github.com>
2025-07-28 11:15:16 +0530

    End of section 9

---
+++ client/src/app/features/shop/product-item/product-item.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input } from '@angular/core';
+import { Product } from '../../../shared/models/product';
+import { CurrencyPipe } from '@angular/common';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-product-item',
+  standalone: true,
+  imports: [
+    MatCardModule,
+    MatBadgeModule,
+    MatButtonModule,
+    MatIconModule,
+    CurrencyPipe
+  ],
+  templateUrl: './product-item.component.html',
+  styleUrls: ['./product-item.component.scss']
+})
+export class ProductItemComponent {
+  @Input() product?: Product;
+}

90ac703c455d242d778f36ce4abf750aa9fa585d
Aditya-9936 <digumarthy.aditya@vector97.com>
2026-01-29 16:02:52 +0530

    End of section 17

--- Core/Interfaces/ISpecification.cs
+++ Core/Interfaces/ISpecification.cs
@@ -8,6 +8,8 @@ public interface ISpecification<T>
     Expression<Func<T, bool>>? Criteria { get; }
     Expression<Func<T, object>>? OrderBy { get; }
     Expression<Func<T, object>>? OrderByDescending { get; }
+    List<Expression<Func<T, object>>> Includes { get; }
+    List<string> IncludeStrings { get; }  // for ThenInclude
     bool IsDistinct { get; }
     int Take { get; }
     int Skip { get; }
@@ -18,5 +20,5 @@ public interface ISpecification<T>
 
 public interface ISpecification<T, TResult> : ISpecification<T>
 {
-    Expression<Func<T , TResult>>? Select { get; }
+    Expression<Func<T, TResult>>? Select { get; }
 }

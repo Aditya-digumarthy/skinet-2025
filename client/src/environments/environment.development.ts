bbd463f9849a4a13b3323247af1303adbc93ac3b
Aditya-9936 <digumarthy.aditya@vector97.com>
2026-01-30 15:52:02 +0530

    End of section 18

--- client/src/environments/environment.development.ts
+++ client/src/environments/environment.development.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: false,
     apiUrl: 'http://localhost:5220/api/',
+    hubUrl: 'http://localhost:5220/hub/notifications',
     stripePublicKey:'pk_test_Aditest'
 };
